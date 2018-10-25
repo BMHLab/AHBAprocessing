c4b34b08f89d2bdc7ec4d393d39231b7093be143
Aurina Arnatkeviciute <aurina.arnatk@gmail.com>
2018-10-25 13:27:03 +1100

    default options changed in processing pipeline script

--- code/dataProcessing/processingPipeline.m
+++ code/dataProcessing/processingPipeline.m
@@ -16,13 +16,13 @@ options.ExcludeCBandBS = true;
 options.useCUSTprobes = true;
 options.updateProbes = 'reannotator';
 options.probeSelections = {'RNAseq'};
-options.parcellations = {'cust100'};
-options.divideSamples = 'ontology'; %'listCortex'; % 'ontology' - GM volume distances are pre-calculated only for listCortex option; 
+options.parcellations = {'aparcaseg'};
+options.divideSamples = 'listCortex'; 
 options.excludeHippocampus = false;
 options.distanceThreshold = 2;
-options.signalThreshold = 0.5; % -1 implies no IBF filtering
+options.signalThreshold = 0.5;
 options.VARfilter = false; 
-options.VARscale = 'normal'; % 'log2', 'normal'
+options.VARscale = 'normal';
 options.VARperc = 50;
 options.RNAsignThreshold = false;
 options.RNAseqThreshold = 0.2;
@@ -30,8 +30,8 @@ options.correctDistance = false;
 options.calculateDS = true;
 options.distanceCorrection = 'Euclidean';
 options.Fit = {'exp'};
-options.normaliseWhat = 'LRcortex'; % what part of the brain is normalised
-options.normMethod = 'scaledRobustSigmoid'; % what type of normalisation method used
+options.normaliseWhat = 'Lcortex'; 
+options.normMethod = 'scaledRobustSigmoid';
 options.percentDS =  100;
 options.doNormalise = true;
 options.saveOutput = true;
@@ -39,7 +39,7 @@ options.normaliseWithinSample = true;
 options.xrange = [0 220];
 options.plotCGE = true;
 options.plotResiduals = true;
-options.meanSamples = 'meanSamples'; %'meanSamples'; meanSubjects
+options.meanSamples = 'meanSamples';
 
 %-------------------------------------------------------------------------------
 % Extract data from excel files and saves to Matlab format:
